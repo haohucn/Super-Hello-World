05dca77f8f92abfff7d444e49a9f20d29ae51275
D058856 <hao.hu@sap.com>
2017-01-16 11:15:04 +0100

    TEST

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -23,7 +23,7 @@ class ViewController: UIViewController {
 
 
     @IBAction func buttonDidClick(_ sender: Any) {
-        helloLabel.text = "Super Hello, world"
+        helloLabel.text = "Super Hello, world. TEST!"
     }
 }
 
